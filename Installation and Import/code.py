974ad89967a27b51bc95d25b6f20ba41b923c1c3
fijjj <123882707+jackhuang0706@users.noreply.github.com>
2024-10-25 09:27:02 +0800

    Update code.py
    加入註解(2024/10/25)

--- Installation and Import/code.py
+++ Installation and Import/code.py
@@ -1,17 +1,16 @@
-!pip install -q -U google-generativeai
+!pip install -q -U google-generativeai #使用AI服務(Google機器人)
 !pip install discord
-!pip install nest_asyncio
-!pip install pytesseract pillow discord
-!apt-get install tesseract-ocr
+!pip install nest_asyncio #允許在異步編程環境中嵌套異步事件循環
+!pip install pytesseract pillow discord 
+!apt-get install tesseract-ocr 
 !apt-get install tesseract-ocr-chi-sim tesseract-ocr-chi-tra  # 安裝中文語言包
-# weather
-import requests
-# picture chatting
+
+import requests #取得天氣
 import discord
-import os
+import os #系統變數
 from PIL import Image
 import pytesseract
-import asyncio
-import nest_asyncio # import nest_asyncio to allow nested event loops
-import google.generativeai as genai
-%env API_KEY=YOUR_API_KEY
+import asyncio #處理異步操作
+import nest_asyncio #允許嵌套異步事件循環
+import google.generativeai as genai #引入AI互動
+%env API_KEY=YOUR_API_KEY #更換為實際的Google AI API
