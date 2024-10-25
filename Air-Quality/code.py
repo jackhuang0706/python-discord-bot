a68d4bfe4b7585c6d915e8b3be178d7d120a637a
fijjj <123882707+jackhuang0706@users.noreply.github.com>
2024-10-25 09:35:43 +0800

    Update code.py
    加上註解(2024/10/25)

--- Air-Quality/code.py
+++ Air-Quality/code.py
@@ -1,13 +1,13 @@
 # 定義查詢空氣品質的函數
 def get_air_quality(address):
     result = '找不到空氣品質資訊'  # 預設結果為找不到資訊
-    code = 'CWA-66C0912B-B99B-436E-B63B-8005F28532C8'
+    code = 'YOUR-CODE' # 替換成實際的授權碼(環保署)
     try:
-        # 空氣品質查詢網址
+        # 空氣品質查詢
         url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
-        req = requests.get(url)  # 發送請求取得空氣品質資料
-        data = req.json()        # 轉換為 JSON 格式
-        records = data['records']  # 取得所有空氣品質紀錄
+        req = requests.get(url) 
+        data = req.json()        
+        records = data['records']  # 所有空氣品質紀錄
         for item in records:
             county = item['county']  # 縣市名稱
             sitename = item['sitename']  # 監測站名稱
@@ -15,10 +15,10 @@ def get_air_quality(address):
             # 將 AQI 分級轉為描述
             aqi_status = ['良好', '普通', '對敏感族群不健康', '對所有族群不健康', '非常不健康', '危害']
             msg = aqi_status[aqi // 50]
-            # 如果地址符合縣市或監測站名稱，則組合回應文字
+            # 組合回應文字
             if county in address or sitename in address:
                 result = f'「{address}」的空氣品質（AQI）：{aqi}，狀態：{msg}。'
                 break
     except Exception as e:
-        print(e)  # 打印錯誤訊息以便除錯
-    return result  # 返回空氣品質資訊
+        print(e)  # 除錯用
+    return result
