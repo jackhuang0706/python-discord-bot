bc28ddea0df843280316dcdb288acd7e0ad20974
fijjj <123882707+jackhuang0706@users.noreply.github.com>
2024-10-25 09:29:34 +0800

    Update code.py
    加上註解(2024/10/25)

--- Earthquake/code.py
+++ Earthquake/code.py
@@ -1,16 +1,16 @@
 # 定義查詢地震資訊的函數
 def earth_quake():
     result = []
-    code = 'CWA-66C0912B-B99B-436E-B63B-8005F28532C8'
+    code = 'YOUR-CODE' #替換成實際的API授權碼(中央氣象局)
     try:
-        # 小區域地震資料查詢網址
+        # 小區域地震
         url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
         req1 = requests.get(url)
         data1 = req1.json()
         eq1 = data1['records']['Earthquake'][0]
         t1 = eq1['EarthquakeInfo']['OriginTime']
 
-        # 顯著有感地震資料查詢網址
+        # 有感地震
         url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
         req2 = requests.get(url2)
         data2 = req2.json()
